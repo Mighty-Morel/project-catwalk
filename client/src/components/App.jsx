44e5c51ca557bf44f1018754189b4451e4e82dc2
Katherine <chiakyu@gmail.com>
2021-09-09 17:55:22 -0700

    reformat for eslint

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import ProductInfo from './ProductInfo.jsx';
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <ProductInfo />
+      </div>
+    );
+  }
+}
+
+export default App;
