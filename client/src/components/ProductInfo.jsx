d266f313e970eb72c54420d9b211bc67ec9c9fea
Katherine <chiakyu@gmail.com>
2021-09-09 23:30:17 -0700

    complete get requests from client

--- client/src/components/ProductInfo.jsx
+++ client/src/components/ProductInfo.jsx
@@ -1,46 +1,84 @@
 /* eslint-disable import/extensions */
 import React from 'react';
-import sampleproducts from './sampleproducts.js';
-import samplestyles from './samplestyles.js';
+import axios from 'axios';
+import AUTH_TOKEN from '../config/config.js';
 import Price from './Price.jsx';
+// import sampleproducts from '../data/sampleproducts.js';
+// import samplestyles from '../data/samplestyles.js';
+
+axios.defaults.baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+axios.defaults.headers.common.Authorization = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 class ProductInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: sampleproducts[0],
-      style: samplestyles.results[0], // an object with a style of the specific product
-    };
+      allProducts: [],
+      productIndex: 0,
+      // selectedProduct: sampleproducts[0],
+      selectedProduct: {},
+      allStyles: [],
+      styleIndex: 0,
+      // selectedStyle: samplestyles.results[0], // an object with a style of the specific product
+      selectedStyle: {},
+    }; // an object with a style of the specific product
+    this.getAllProducts = this.getAllProducts.bind(this);
+    this.getAllStyles = this.getAllStyles.bind(this);
     this.updateProduct = this.updateProduct.bind(this);
-    this.updateStyle = this.updateStyle.bind(this);
   }
 
-  updateProduct(productid) {
-    // refactor to pull from api - run this when style changes
-    // product will be chosen based on the style
-    this.setState({
-
-    });
+  // renders the info of the specific product on load
+  componentDidMount() {
+    const { allProducts, productIndex } = this.state;
+    this.getAllProducts();
+    this.updateProduct(allProducts[productIndex].id);
   }
 
-  updateStyle(productid) {
+  getAllProducts() {
+    axios.get('/products')
+      .then((response) => {
+        console.log('get all Products');
+        this.setState({ allProducts: response.data });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  getAllStyles(productId) {
+    const { allStyles, styleIndex } = this.state;
     // refactor to pull from api
-    axios.get();
-    this.setState({
-      product: updateProduct(productid),
-    });
+    axios.get(`/products/${productId}/styles`)
+      .then((response) => {
+        console.log('get all styles', response.data);
+        this.setState({ allStyles: response.data.results });
+      })
+      .then(this.setState({ selectedStyle: allStyles[styleIndex] }))
+      .catch((error) => console.log(error));
+  }
+
+  updateProduct(productId) {
+    console.log('productId', productId);
+    axios.get(`/products/${productId}`)
+      .then((response) => {
+        console.log('get product info', response.data);
+        this.setState({
+          selectedProduct: response.data,
+        });
+      })
+      .then(this.getAllStyles(productId))
+      .catch((error) => console.log(error));
   }
 
   render() {
-    const { product, discount, style } = this.state;
+    const { selectedProduct, selectedStyle } = this.state;
     return (
       <>
         <span>Social Media Placeholder</span>
-        <h5>{product.category}</h5>
-        <h4>{product.name}</h4>
-        <Price style={style} />
+        <h5>{selectedProduct.category}</h5>
+        <h4>{selectedProduct.name}</h4>
+        <Price style={selectedStyle} />
         <br />
-        <span>{product.description}</span>
+        <span>{selectedProduct.description}</span>
         <br />
         <span>Social Media Placeholder</span>
       </>
