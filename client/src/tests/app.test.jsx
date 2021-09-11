99fe5a9949e8b1ff899ef555bbc0944afd107601
Katherine <chiakyu@gmail.com>
2021-09-10 18:06:25 -0700

    clean up comments and unused files

--- client/src/tests/app.test.jsx
+++ client/src/tests/app.test.jsx
@@ -2,41 +2,17 @@
  * @jest-environment jsdom
  */
 
-// app.test.js
+// app.test.jsx - USE THIS AS THE SAMPLE
+
 // See https://reactjs.org/docs/testing-recipes.html for more testing examples
 // And here: https://testing-library.com/docs/react-testing-library/example-intro/
 import React from 'react';
-// import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import App from '../components/App';
 
-// let container = null;
-// beforeEach(() => {
-//   // setup a DOM element as a render target
-//   container = document.createElement('div');
-//   document.body.appendChild(container);
-// });
-
-// afterEach(() => {
-//   // cleanup on exiting
-//   unmountComponentAtNode(container);
-//   container.remove();
-//   container = null;
-// });
-
-// it('renders on load', () => {
-//   act(() => {
-//     render(<App />, container);
-//   });
-//   expect(container.textContent).toBe('Hello World!');
-// });
-console.log(typeof App);
-
 test('renders on load', () => {
-  const { getByText } = render(<App />)
-  expect(getByText('Hello World!')).toBeInTheDocument()
-  // expect(getByTestId(<div))
+  const { getByText } = render(<App />);
+  expect(getByText('Hello World!')).toBeInTheDocument();
 });
