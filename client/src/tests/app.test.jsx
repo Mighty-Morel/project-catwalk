881cfdbbf205714148d5e2b2e329e8d9b34e5463
Katherine <chiakyu@gmail.com>
2021-09-10 17:39:49 -0700

    update tests

--- client/src/tests/app.test.jsx
+++ client/src/tests/app.test.jsx
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 // app.test.js
 // See https://reactjs.org/docs/testing-recipes.html for more testing examples
 import React from 'react';
@@ -6,23 +10,30 @@ import { act } from 'react-dom/test-utils';
 
 import App from '../components/App';
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
+// let container = null;
+// beforeEach(() => {
+//   // setup a DOM element as a render target
+//   container = document.createElement('div');
+//   document.body.appendChild(container);
+// });
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+// afterEach(() => {
+//   // cleanup on exiting
+//   unmountComponentAtNode(container);
+//   container.remove();
+//   container = null;
+// });
+
+// it('renders on load', () => {
+//   act(() => {
+//     render(<App />, container);
+//   });
+//   expect(container.textContent).toBe('Hello World!');
+// });
+console.log(typeof App);
 
 it('renders on load', () => {
-  act(() => {
-    render(<App />, container);
-  });
-  expect(container.textContent).toBe('Hello World!');
+  render(<App />)
+  expect(getByText('Hello World!')).toBeInTheDocument();
+  // expect(getByTestId(<div))
 });
