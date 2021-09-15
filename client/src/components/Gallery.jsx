f4a2a3d7cb0c894072aaebb0746c04c7b3af94bb
Katherine <chiakyu@gmail.com>
2021-09-15 16:04:06 -0700

    create selections for main image and thumbnails

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,38 +1,44 @@
 /* eslint-disable import/extensions */
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateProductInfo } from '../reducers/Example-Reducer';
-import { updateStyles } from '../reducers/Style-Reducer';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import Price from './Price.jsx';
-import StyleSelector from './StyleSelector.jsx';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateStyleId } from '../reducers/Style-Reducer';
-
-const Gallery = ({ style }) => {
-  const dispatch = useDispatch();
-  const styleId = style.style_id;
-
-  const handleClick = () => {
-    console.log(styleId);
-    dispatch(updateStyleId(styleId));
-  };
-
+const Gallery = () => {
+  const allStyles = useSelector((state) => state.style.allStyles);
   const selectedStyleId = useSelector((state) => state.style.id);
 
+  // find the style selected
+  const selectedStyle = allStyles.find((style) => selectedStyleId === style.style_id);
+  const allPhotos = selectedStyle.photos;
+
+  const [mainImage, setImage] = useState(allPhotos[0].url);
+
+  const handleClick = (e) => {
+    const selectedImage = e.target.value;
+    setImage(selectedImage.url);
+  };
+
   return (
-    <span data-testid="click-style" role="button" tabIndex="-1" onClick={handleClick} onKeyPress={handleClick}>
-      <img
-        className={style.style_id === selectedStyleId ? 'style-selected' : 'style-unselected'}
-        src={style.photos[0].thumbnail_url}
-        alt={style.name}
-        title={style.name}
-      />
-      <p className="style-name">{style.name}</p>
-    </span>
+    <>
+      <div className="main-image-area">
+        <img
+          className="main-image"
+          src={mainImage.url}
+          alt={selectedStyle.name}
+          title={selectedStyle.name}
+        />
+      </div>
+      <div className="thumbnails">
+        {allPhotos.map((photo) => (
+          <span role="menuitem" tabIndex="-1" onClick={handleClick} onKeyPress={handleClick}>
+            <img
+              src={photo.thumbnail_url}
+              alt={selectedStyle.name}
+            />
+          </span>
+        ))}
+      </div>
+    </>
   );
 };
 
 export default Gallery;
-
