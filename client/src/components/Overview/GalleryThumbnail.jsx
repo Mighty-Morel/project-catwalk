4a9447564b0a82be7db21caaa50f85719b09becf
Katherine <chiakyu@gmail.com>
2021-09-16 23:54:20 -0700

    add css formatting to highlight selected thumbnail

--- client/src/components/Overview/GalleryThumbnail.jsx
+++ client/src/components/Overview/GalleryThumbnail.jsx
@@ -1,23 +1,33 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable import/extensions */
 import React from 'react';
+import { useSelector } from 'react-redux';
 // eslint-disable-next-line no-unused-vars
 import overviewStyling from './overview.css';
 
 const GalleryThumbnail = ({ photo, style, changePhoto }) => {
+  const selectedPhotoUrl = useSelector((state) => state.style.mainPhoto.url);
+
   const handleClick = () => {
     changePhoto(photo);
   };
 
+  const renderThumbnail = () => {
+    if (selectedPhotoUrl === photo.url) {
+      return (
+        <>
+          <img className="image-thumbnail" id="selected-thumbnail" src={photo.thumbnail_url} alt={style.name} />
+          <hr />
+        </>
+      );
+    }
+    return <img className="image-thumbnail" src={photo.thumbnail_url} alt={style.name} />;
+  };
+
   return (
     <span role="menuitem" tabIndex="-1" onClick={handleClick} onKeyPress={handleClick}>
-      <img
-        className="image-thumbnail"
-        src={photo.thumbnail_url}
-        alt={style.name}
-      />
+      {renderThumbnail()}
     </span>
-
   );
 };
 
