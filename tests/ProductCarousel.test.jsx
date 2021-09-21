855171dc3730e49c2465bf7456d1142c0dc03423
Robin Jeng <robinjeng0@gmail.com>
2021-09-20 21:19:19 -0700

    Delete duplicate product carousel test, create new product carousel test, built partial setup

--- tests/ProductCarousel.test.jsx
+++ tests/ProductCarousel.test.jsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-// Product Info Tests ==============================================
+// Product Carousel Tests ==============================================
 
 import React from 'react';
 import {
@@ -11,5 +11,44 @@ import {
 import '@testing-library/jest-dom';
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
+
 import ProductCarousel from '../client/src/components/Related/ProductCarousel';
-import Modal from '../client/src/components/Modal/';
+import Modal from '../client/src/components/Related/Modal';
+import RelatedItems from '../client/src/components/Related/RelatedItems';
+
+beforeAll(() => {
+  axios.get.mockImplementation((url) => {
+    switch (url) {
+      case '/products/1':
+        return Promise.resolve(mockProductData);
+      case '/products/1/related':
+        return Promise.resolve(mockRelatedIds);
+      default:
+        return Promise.reject(new Error('Error - this test is not working'));
+    }
+  });
+});
+
+afterEach(cleanup);
+
+jest.mock('../client/src/components/Related/card.css', () => () => (<div>Carousel Card Style Placeholder</div>));
+jest.mock('../client/src/components/Related/carousel.css', () => () => (<div>Carousel Style Placeholder</div>));
+jest.mock('../client/src/components/Related/modal.css', () => () => (<div>PlModal Style Placeholder</div>));
+
+const mockProductData = [
+  {
+    id: 1,
+    name: 'Camo Onesie',
+    slogan: 'Blend in to your crowd',
+    description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+    category: 'Jackets',
+    default_price: '140',
+  },
+];
+
+const mockRelatedIds = [
+  2,
+  3,
+  8,
+  7,
+];
