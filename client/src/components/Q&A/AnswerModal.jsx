2f27ac2684cc93c1071f640c157464d17b2ebcde
alan_fong <alanfong@berkeley.edu>
2021-09-21 11:53:23 -0700

    Formatted answer modal
    Implemented checks that mandatory inputs have been filled out

--- client/src/components/Q&A/AnswerModal.jsx
+++ client/src/components/Q&A/AnswerModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import styling from './questions.css';
@@ -11,6 +11,9 @@ const AnswerModal = (props) => {
   const [nameInput, setNameInput] = useState('sample');
   const [emailInput, setEmailInput] = useState('sample@yahoo.com');
   const [imageInput, setImageInput] = useState([]);
+  const [textCheck, setTextCheck] = useState(false);
+  const [nameCheck, setNameCheck] = useState(false);
+  const [emailCheck, setEmailCheck] = useState(false);
 
   // Destructuring
   const { toggleAnswerForm, id, question } = props;
@@ -18,14 +21,29 @@ const AnswerModal = (props) => {
   // Input change handlers
   const textChangeHandler = (event) => {
     setTextInput(event.target.value);
+    if (event.target.value.length > 0) {
+      setTextCheck(true);
+    } else {
+      setTextCheck(false);
+    }
   };
 
   const nameChangeHandler = (event) => {
     setNameInput(event.target.value);
+    if (event.target.value.length > 0) {
+      setNameCheck(true);
+    } else {
+      setNameCheck(false);
+    }
   };
 
   const emailChangeHandler = (event) => {
     setEmailInput(event.target.value);
+    if (event.target.value.indexOf('@') > -1) {
+      setEmailCheck(true);
+    } else {
+      setEmailCheck(false);
+    }
   };
 
   // post answer information to API
@@ -36,46 +54,62 @@ const AnswerModal = (props) => {
       email: emailInput,
       photos: imageInput,
     };
-    axios.post(`/qa/questions/${id}/answers`, answerInfo)
-      .then(() => {
-        toggleAnswerForm();
-      })
-      .catch((error) => {
-        console.log('error in posting answer on the client', error);
-      });
+    if (textCheck && nameCheck && emailCheck) {
+      axios.post(`/qa/questions/${id}/answers`, answerInfo)
+        .then(() => {
+          toggleAnswerForm();
+        })
+        .catch((error) => {
+          console.log('error in posting answer on the client', error);
+        });
+    } else {
+      alert('You must contain the following: answer, nickname, and email.');
+    }
   };
 
   // Get product name
-  axios.get(`/products/${currentId}`)
-    .then((response) => {
-      setProductName(response.data.name);
-    })
-    .catch((error) => {
-      console.log('error in getting product name on the client side', error);
-    });
+  const getProductName = () => {
+    axios.get(`/products/${currentId}`)
+      .then((response) => {
+        setProductName(response.data.name);
+      })
+      .catch((error) => {
+        console.log('error in getting product name on the client side', error);
+      });
+  };
+
+  useEffect(getProductName, [currentId]);
 
   return (
     <div className="modal">
       <div className="modal-content">
         <div className="modal-header">
           <h4 className="modal-title">Submit Your Answer</h4>
-          <span>"{productName}: {question}"</span>
+          <span>
+            &#34;
+            {productName}
+            :&#160;
+            {question}
+            &#34;
+          </span>
         </div>
         <form>
           <div className="modal-body">
             <span>Your Answer:</span>
             <br />
-            <input type="text" id="question-entry" size="100" maxLength="1000" onChange={textChangeHandler} />
+            <textarea type="text" id="modal-textbox" maxLength="1000" onChange={textChangeHandler} />
+            <br />
             <br />
             <span>Name: </span>
-            <input type="text" maxLength="60" placeholder="Example:jack543!" onChange={nameChangeHandler} />
+            <input type="text" size="60" maxLength="60" placeholder="Example: jack543!" onChange={nameChangeHandler} />
             <br />
             <span><em>For privacy reasons, do not use your full name or email address</em></span>
             <br />
-            <span>Email: </span>
-            <input type="text" maxLength="60" placeholder="jack@email.com" onChange={emailChangeHandler} />
             <br />
-            <span>For authentication reasons, you will not be emailed</span>
+            <span>Email: </span>
+            <input type="text" size="60" maxLength="60" placeholder="jack@email.com" onChange={emailChangeHandler} />
+            <br />
+            <span><em>For authentication reasons, you will not be emailed</em></span>
           </div>
           <div className="modal-footer">
             <button type="button" onClick={toggleAnswerForm}>Close</button>
