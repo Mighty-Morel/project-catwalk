f48d8318f470771cde6e72e7b9ec48634c5d4017
Robin Jeng <robinjeng0@gmail.com>
2021-09-21 16:13:01 -0700

    Code clean-up

--- client/src/components/Related/ProductCarousel.jsx
+++ client/src/components/Related/ProductCarousel.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable import/extensions */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/jsx-one-expression-per-line */
