a2b6b652fa127a8be8484a42bf63b6c06df5d2c5
Katherine <chiakyu@gmail.com>
2021-09-22 20:48:47 -0700

    fix cart tests

--- tests/cart.test.jsx
+++ tests/cart.test.jsx
@@ -14,11 +14,6 @@ import { Provider } from 'react-redux';
 import store from '../client/src/store/store';
 import App from '../client/src/components/App';
 import AddToCartFeatures from '../client/src/components/Overview/AddToCart';
-import
-{
-  mockProductData, mockCartData, mockStyleData, mockStyle,
-}
-  from './fixtures/mockData';
 
 jest.mock('../client/src/components/Overview/overview.css', () => () => (<div>Placeholder Overview Style</div>));
 jest.mock('../client/src/components/Overview/ProductInfo', () => () => (<div>Placeholder Product Info</div>));
@@ -30,6 +25,76 @@ jest.mock('../client/src/components/Reviewlist/reviewlist.css', () => () => (<di
 jest.mock('../client/src/components/Reviewlist/Review-list', () => () => (<div>Placeholder Review List</div>));
 jest.mock('../client/src/reducers/Review-List-Slice.js', () => () => (<div>Review List Slice Placeholder</div>));
 
+// MOCK DATA FOR TESTS =========================================================
+const mockProductData = {
+  id: 48432,
+  name: 'Morning Joggers',
+  slogan: 'Make yourself a morning person',
+  description: "Whether you're a morning person or not. Whether you're gym bound or not. Everyone looks good in joggers.",
+  category: 'Pants',
+  default_price: '40',
+};
+
+const mockStyleData = {
+  product_id: '48432',
+  results: [
+    {
+      style_id: 293480,
+      name: 'Desert Brown & Tan',
+      original_price: '140.00',
+    },
+    {
+      style_id: 123456,
+      name: 'Testing only',
+      original_price: '0',
+    },
+    {
+      style_id: 654321,
+      name: 'Testing 2',
+      original_price: '10.00',
+    },
+  ],
+};
+
+const mockStyle = {
+  style_id: 293480,
+  name: 'Forest Green & Black',
+  original_price: '140.00',
+  sale_price: null,
+  'default?': true,
+  photos: [{
+    thumbnail_url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+    url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
+  },
+  {
+    thumbnail_url: 'https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+    url: 'https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80',
+  }],
+  skus: {
+    1702764: { quantity: 8, size: 'XS' },
+    1702765: { quantity: 16, size: 'S' },
+    1702766: { quantity: 17, size: 'M' },
+    1702767: { quantity: 10, size: 'L' },
+    1702768: { quantity: 15, size: 'XL' },
+  },
+};
+
+const mockCartData = [
+  {
+    sku_id: 1702764,
+    count: '1',
+  },
+  {
+    sku_id: 1702799,
+    count: '3',
+  },
+  {
+    sku_id: 1702925,
+    count: '7',
+  },
+];
+
+// SETUP MOCK SERVER =============================================================
 // declare which API requests to mock
 const server = setupServer(
   rest.get('/products/48432', (req, res, ctx) => res(ctx.json(mockProductData))),
@@ -40,6 +105,8 @@ const server = setupServer(
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+// TESTS =============================================================
 
 test('renders App on load', async () => {
   render(
@@ -119,14 +186,10 @@ test('quantity dropdown is no longer disabled when a size is selected', async ()
 // };
 
 // it('should add the selected size and quantity to the cart', async () => {
-//   // const item = {
-//   //   sku_id: 123456,
-//   //   count: 20,
-//   // };
-
 //   const { qtyInput } = setup();
 //   fireEvent.change(qtyInput, { target: { value: '10' } });
 //   expect(qtyInput.value).toBe('10');
 
-//   // fireEvent.click(sizeInput, { target: { value: 'M' } });
-//   // expect(sizeInput.value).toBe('M');
+//   fireEvent.click(sizeInput, { target: { value: 'M' } });
+//   expect(sizeInput.value).toBe('M');
+// }
