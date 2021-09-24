c8da9fcf23e667a33f85d249b21be1ddc14e8334
Katherine <chiakyu@gmail.com>
2021-09-24 14:50:40 -0700

    tried to update with zoom but lost images

--- client/src/components/Overview/Gallery.jsx
+++ client/src/components/Overview/Gallery.jsx
@@ -13,6 +13,7 @@ const Gallery = () => {
   const expandedView = useSelector((state) => state.product.expandedView);
 
   const [mainPhotoIndex, setPhotoIndex] = useState(0);
+  const [zoomView, setZoomView] = useState(false);
 
   const mainImage = stylePhotos[mainPhotoIndex];
 
@@ -44,7 +45,7 @@ const Gallery = () => {
   const renderUpArrow = () => {
     if (stylePhotos.length > 7 && mainPhotoIndex > 0) {
       return (
-        <span role="button" tabIndex="-1" onClick={moveUp} onKeyDown={handleKeyDown}>
+        <span role="button" tabIndex="-1" onClick={() => moveUp()} onKeyDown={() => handleKeyDown()}>
           <img className="overview-thumbnail-arrow" alt="up arrow" src="./images/up-arrow.png" />
         </span>
       );
@@ -55,7 +56,7 @@ const Gallery = () => {
   const renderDownArrow = () => {
     if (stylePhotos.length > 7 && mainPhotoIndex < stylePhotos.length - 1) {
       return (
-        <span role="button" tabIndex="-1" onClick={moveDown} onKeyDown={handleKeyDown}>
+        <span role="button" tabIndex="-1" onClick={() => moveDown()} onKeyDown={() => handleKeyDown()}>
           <img className="overview-thumbnail-arrow" alt="down arrow" src="./images/down-arrow.png" />
         </span>
       );
@@ -66,7 +67,7 @@ const Gallery = () => {
   const renderLeftArrow = () => {
     if (mainPhotoIndex > 0) {
       return (
-        <span role="button" tabIndex="-1" onClick={moveUp} onKeyDown={handleKeyDown}>
+        <span role="button" tabIndex="-1" onClick={() => moveUp()} onKeyDown={() => handleKeyDown()}>
           <img className="overview-main-arrow left" alt="left arrow" src="./images/double-left-arrow.png" />
         </span>
       );
@@ -77,7 +78,7 @@ const Gallery = () => {
   const renderRightArrow = () => {
     if (mainPhotoIndex < stylePhotos.length - 1) {
       return (
-        <span role="button" tabIndex="-1" onClick={moveDown} onKeyDown={handleKeyDown}>
+        <span role="button" tabIndex="-1" onClick={() => moveDown()} onKeyDown={() => handleKeyDown()}>
           <img className="overview-main-arrow right" alt="right arrow" src="./images/double-right-arrow.png" />
         </span>
       );
@@ -86,7 +87,84 @@ const Gallery = () => {
   };
 
   const toggleView = () => {
-    dispatch(updateView(!expandedView));
+    dispatch(() => updateView(true));
+  };
+
+  const toggleZoom = () => {
+    dispatch(() => setZoomView(!zoomView));
+  };
+
+  const renderMainImage = () => {
+    if (zoomView) {
+      return (
+        <div
+          className="overview-main-image-zoom"
+          onClick={() => toggleZoom()}
+          onKeyPress={() => toggleZoom()}
+          onMouseEnter={() => dragElement()}
+          role="link"
+          tabIndex="-1"
+        >
+          <img src={mainImage.url} alt={selectedStyle.name} title={selectedStyle.name} />
+        </div>
+      );
+    }
+    if (expandedView) {
+      return (
+        <div
+          className="overview-main-image-expanded"
+          onClick={() => toggleZoom()}
+          onKeyPress={() => toggleZoom()}
+          onMouseEnter={() => dragElement()}
+          role="link"
+          tabIndex="-1"
+        >
+          <img src={mainImage.url} alt={selectedStyle.name} title={selectedStyle.name} />
+        </div>
+      );
+    }
+    return (
+      <div className="overview-main-image">
+        <img src={mainImage.url} alt={selectedStyle.name} title={selectedStyle.name} />
+      </div>
+    );
+  };
+
+  // zoom and drag function
+  let pos1 = 0; let pos2 = 0; let pos3 = 0; let pos4 = 0;
+
+  const dragElement = (elmnt) => {
+    const elementDrag = (e) => {
+      // e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = `${elmnt.offsetTop - pos2}px`;
+      elmnt.style.left = `${elmnt.offsetLeft - pos1}px`;
+    };
+
+    const dragMouseDown = (e) => {
+      // e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    };
+
+    elmnt.onmousedown = dragMouseDown;
+
+    const closeDragElement = () => {
+    // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    };
   };
 
   if (!mainImage) {
@@ -95,13 +173,20 @@ const Gallery = () => {
   return (
     <>
       <div className="overview-gallery-container">
-        <div className="overview-main-image-container" onClick={toggleView}>
-          <img
-            className={expandedView? "overview-main-image-expanded" : "overview-main-image"}
+        <div
+          className="overview-main-image-container"
+          onClick={() => toggleView()}
+          onKeyPress={() => toggleView()}
+          role="link"
+          tabIndex="-1"
+        >
+          {renderMainImage()}
+          {/* <img
+            className={if (expandedView) ? "overview-main-image-expanded" : "overview-main-image"}
             src={mainImage.url}
             alt={selectedStyle.name}
             title={selectedStyle.name}
-          />
+          /> */}
         </div>
         <div className="overview-thumbnail-container">
           {renderUpArrow()}
