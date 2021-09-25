3c8a4fbadf35391c430bb2461754ce093ee2cbd2
Katherine <chiakyu@gmail.com>
2021-09-24 23:42:16 -0700

    add file loader to handle image files

--- webpack.common.js
+++ webpack.common.js
@@ -30,7 +30,14 @@ module.exports = {
           },
         },
       },
-
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
 };
